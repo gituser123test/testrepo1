6e99a0c671cbd6eadb6f4c83d1ae809cb8b6d4af
gituser123test <testmail123user@gmail.com>
2017-11-28 15:51:49 +0530

    edit new page for auto ui 226

--- test2/scriptexec.php
+++ test2/scriptexec.php
@@ -1,4 +1,3 @@
 <?php
-echo "Button Click";
-shell_exec("admin:admin@master1:/home/admin/Desktop/scripts/aem_env_auto1.sh");
+shell_exec('admin@master1:/home/admin/Desktop/scripts/aem_env_auto1.sh');
 ?>
