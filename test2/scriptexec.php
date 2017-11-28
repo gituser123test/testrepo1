d7e89850c503b29da47a8fb01388e8d455dd7f63
gituser123test <testmail123user@gmail.com>
2017-11-28 14:29:19 +0530

    edit new page for auto ui 224

--- test2/scriptexec.php
+++ test2/scriptexec.php
@@ -1,5 +1,5 @@
 <?php
 echo "Button Click";
 shell_exec("admin:admin@master1:/home/admin/Desktop/scripts/aem_env_auto1.sh");
-header('Location: /test2/autodeployer.html?success=true');
+header('Location: /autodeployer.html?success=true');
 ?>
