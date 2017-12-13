6659740fd27d6c2c81d264d85602b67ba90b0b93
gituser123test <testmail123user@gmail.com>
2017-12-13 12:35:13 +0530

    playbook 107

--- test2/scriptexec.php
+++ test2/scriptexec.php
@@ -1,14 +1,3 @@
 <?php
-if($_GET){
-    if(isset($_GET['submit'])){
-        execute();
-    }
-}
- 
-    function execute()
-    {
-       shell_exec("ssh admin@master1 /home/admin/Desktop/scripts/aem_env_auto1.sh");
-    }
-    
- 
+shell_exec("ssh admin@master1 /home/admin/Desktop/scripts/aem_env_auto1.sh");
 ?>
