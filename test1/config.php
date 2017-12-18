94001d75108b645df4c4cd1e76c377e1a2dbde25
gituser123test <testmail123user@gmail.com>
2017-12-18 14:41:43 +0530

    test1_1021

--- test1/config.php
+++ test1/config.php
@@ -1,3 +1,5 @@
 <?php
-shell_exec("ssh admin@master1 /home/admin/Desktop/scripts/azure_config.sh");
+shell_exec("ssh admin@master1 /home/admin/Desktop/scripts/azure_config1.sh &>/dev/null &");
+shell_exec("ssh admin@master1 /home/admin/Desktop/scripts/azure_config2.sh &>/dev/null &");
+return;
 ?>
--- test1/create.php
+++ test1/create.php
@@ -1,3 +1,5 @@
 <?php
-shell_exec("ssh admin@master1 /home/admin/Desktop/scripts/azure_create.sh");
+shell_exec("ssh admin@master1 /home/admin/Desktop/scripts/azure_create1.sh &>/dev/null &");
+shell_exec("ssh admin@master1 /home/admin/Desktop/scripts/azure_create2.sh &>/dev/null &");
+return;
 ?>
